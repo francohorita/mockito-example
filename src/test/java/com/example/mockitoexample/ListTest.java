9fcf72a3b66074b8910718b5795280d518f426b1
Franco Horita <franco.horita@gmail.com>
2019-06-21 12:48:36 -0300

    Mocking parameters and more

---
+++ src/test/java/com/example/mockitoexample/ListTest.java
@@ -0,0 +1,45 @@
+package com.example.mockitoexample;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ListTest {
+    @Test
+    public void sizeTest(){
+        List listMock = mock(List.class);
+        when(listMock.size()).thenReturn(10);
+        assertEquals(10, listMock.size());
+        assertEquals(10, listMock.size());
+    }
+
+    @Test
+    public void sizeMultipleReturnsTest(){
+        List listMock = mock(List.class);
+        when(listMock.size()).thenReturn(10).thenReturn(20);
+        assertEquals(10, listMock.size());
+        assertEquals(20, listMock.size());
+        assertEquals(20, listMock.size());
+    }
+
+    @Test
+    public void getTest(){
+        List listMock = mock(List.class);
+        when(listMock.get(0)).thenReturn("SomeString");
+        assertEquals("SomeString", listMock.get(0));
+        assertEquals(null, listMock.get(1));
+    }
+
+    @Test
+    public void getGenericParameterTest(){
+        List listMock = mock(List.class);
+        when(listMock.get(Mockito.anyInt())).thenReturn("SomeString");
+        assertEquals("SomeString", listMock.get(0));
+        assertEquals("SomeString", listMock.get(1));
+    }
+}
